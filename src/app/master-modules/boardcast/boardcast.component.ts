77b4f7cd5be624f48fd5166bd9210facc15c2cc2
Yogeshwarp <patil.yogeshwar@gmail.com>
2023-09-21 15:54:30 +0530

    changes 1

--- src/app/master-modules/boardcast/boardcast.component.ts
+++ src/app/master-modules/boardcast/boardcast.component.ts
@@ -16,7 +16,7 @@ export class BoardcastComponent implements OnInit{
   ngOnInit(): void {
     this.fetchVideoDetails();
   }
-/* fatch video  15:38 changes */
+/* fatch video  15:38 changes ok */
   fetchVideoDetails() {
     const backendApiUrl = 'http://143.244.136.201:3001/v1/broadcast?limit=10&page=1';
 
